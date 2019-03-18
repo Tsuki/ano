b262067ee5f3bf684e69a9df4ef13feb71bdff90
Tsukatsuki Kanade <i@sukitsuki.com>
2019-03-18 16:34:23 +0700

    feat: remove const

--- app/src/main/java/com/sukitsuki/ano/AppConst.kt
+++ app/src/main/java/com/sukitsuki/ano/AppConst.kt
@@ -1,11 +1,5 @@
 package com.sukitsuki.ano
 
 object AppConst {
-  const val EbbUserAgent = "ebb Android v1.4.3a (Linux;Android 6.0)"
-  const val FakeUserAgent =
-    "Mozilla/5.0 (Linux; Android 9; ONEPLUS A5000) AppleWebKit/537.36 " +
-      "(KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36"
-  const val BASE_URL = "https://ebb.io/"
-  const val DatabaseName = "TbbDatabase.db"
-  const val TITLE_TAG = "settingsActivityTitle"
+  const val DatabaseName = "ano.db"
 }
