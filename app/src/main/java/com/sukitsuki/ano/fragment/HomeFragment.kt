deef5c428afdcfa8f5212b92d4ebd30fb6173907
Tsukatsuki Kanade <i@sukitsuki.com>
2019-03-19 01:49:43 +0700

    fix: Dagger class

--- app/src/main/java/com/sukitsuki/ano/fragment/HomeFragment.kt
+++ app/src/main/java/com/sukitsuki/ano/fragment/HomeFragment.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.sukitsuki.ano.R
 import com.sukitsuki.ano.utils.ViewModelFactory
 import dagger.android.AndroidInjector
+import dagger.android.support.DaggerFragment
 import timber.log.Timber
 import javax.inject.Inject
 
-class HomeFragment : Fragment() {
+class HomeFragment : DaggerFragment() {
 
   companion object {
     fun newInstance() = HomeFragment()
@@ -41,7 +41,9 @@ class HomeFragment : Fragment() {
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
-    viewModel.favorites.observe(this, Observer { Timber.i("${it[0]}") })
+    viewModel.favorites.observe(this, Observer {
+      if (it.isNotEmpty()) Timber.i("${it[0]}")
+    })
   }
 
 }
